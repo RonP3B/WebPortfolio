cd256335a04a33823dd93ddbf3634d93d3ff06b3
Roniell Pérez <71516416+RonP3B@users.noreply.github.com>
2023-08-30 12:48:59 -0400

    Setting Material UI

--- portafolio/src/main.tsx
+++ portafolio/src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import CssBaseline from "@mui/material/CssBaseline";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
